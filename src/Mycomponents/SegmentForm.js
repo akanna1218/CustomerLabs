a59107f9093495dac2d1f1f0615947b530420493
akanna1218 <ajithkannapandian@gmail.com>
2024-04-02 11:56:25 +0530

    changed save segment text form

--- src/Mycomponents/SegmentForm.js
+++ src/Mycomponents/SegmentForm.js
@@ -6,7 +6,7 @@ const SegmentForm = ({ setSegmentName, segmentName }) => {
 
   return (
     <div className='segment-form'>
-      <label style={{"fontSize":"large"}}>Enter the Name of the Segment</label>
+      <label style={{"fontSize":"medium"}}>Enter the Name of the Segment</label>
       <input type="text" value={segmentName}  placeholder={'Name of the segment'} onChange={(e) => setSegmentName(e.target.value)} />
     <p style={{"fontSize":"16px"}}> To save your segment,you need to add the schemas to build the query</p>
     </div>
